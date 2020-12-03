b407a1c3abf1db32749d28ca8956cc6fc994e246
Emmanuel <fegomson@gmail.com>
2020-12-03 17:34:23 -0600

    configure server .env and middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+
 
 // Start express in app
 const app = express()
+const mongoose = require("mongoose")
+
+// configure database
+require('dotenv').config()
+
+// Setup mongoose
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.DATABASE)
+
+// body and cookie parser middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 // Create the server
 const port = process.env.PORT || 3002
