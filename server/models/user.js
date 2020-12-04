4816d614707dd5b1264ef36c7ed285545a4e94bb
Emmanuel <fegomson@gmail.com>
2020-12-03 19:34:57 -0600

    Server - hashing passwords

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 
+// Encrypt password string
+const bcrypt = require("bcrypt")
+
+const SALT_I = 10;
+
 const userSchema = mongoose.Schema({
     email: {
         type: String,
@@ -36,8 +41,27 @@ const userSchema = mongoose.Schema({
     },
     token: {
         type: String,
-        
+
     }
+})
+
+userSchema.pre('save', function(next) {
+    var user = this
+
+    bcrypt.genSalt(SALT_I, function(err, salt) {
+        if (err) return next(err)
+        // else
+        if (user.isModified()) {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        } else {
+            next()
+        }
+        
+    })
 })
 
 const User = mongoose.model('User', userSchema)
