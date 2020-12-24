9dd9a40a8fb50c20fded0ac3d7a5d8a2ee14d6a9
Emmanuel Egomson <fegomson@gmail.com>
2020-12-24 13:32:41 -0600

    implement the Woods model and routes

--- server/server.js
+++ server/server.js
@@ -30,12 +30,39 @@ app.use(cookieParser())
 //=================
 const {User} = require('./models/user')
 const {Brand} = require('./models/brand')
+const {Wood} = require('./models/wood')
 
 //=================
 // Middlewares
 //=================
 const { auth } =  require('./middleware/auth')
 const { admin } =  require('./middleware/admin')
+
+
+//=================
+// WOODS
+//=================
+
+// Add Wood
+app.post('/api/product/wood', auth, admin, (req, res) => {
+    const wood = new Wood(req.body)
+
+    wood.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        res.status(200).json({
+            success: true,
+            wood: doc
+        })
+    })
+})
+
+// Get all woods
+app.get("/api/product/woods", (req, res) => {
+    Wood.find({}, (err, woods) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).send(woods)
+    })
+})
 
 
 //=================
@@ -60,8 +87,8 @@ app.post('/api/product/brand', auth, admin, (req, res) => {
 app.get('/api/product/brands', (req, res) => {
     Brand.find({}, (err, brands) => {
         if(err) return res.status(400).send(err)
-        res.status(400).send(brands)
-    })    
+        res.status(200).send(brands)
+    })
 })
 
 
