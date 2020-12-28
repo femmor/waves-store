8e94dc60c959c07a9e34695a17b6685b5a857ece
Emmanuel Egomson <fegomson@gmail.com>
2020-12-28 14:35:42 -0600

    Create the RegisterLogin component

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Switch, Route } from "react-router-dom"
-import Home from './components/Home'
 import Layout from './hoc/layout'
+import Home from './components/Home'
+import RegisterLogin from './components/Register_Login'
 const Routes = () => {
     return (
         <Layout>
             <Switch>
                 <Route exact component={Home} path="/"/>
+                <Route component={RegisterLogin} path="/register"/>
             </Switch>
         </Layout>
     )
